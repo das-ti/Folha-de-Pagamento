7f59a4b99b8cb17e8afa3baa4f086bb6ea490581
Douglas Alves <89371510+das-ti@users.noreply.github.com>
2025-05-22 17:56:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function calcular() {
 
   if (nome == "") {
     alert("Digite o nome do funcionário!");
-  } 
-  else {
+  } else {
     for (let i = 0; i < contrato.length; i++) {
       if (contrato[i].checked) {
         porcentagem_desconto = parseFloat(contrato[i].value);
@@ -36,11 +35,23 @@ function calcular() {
       salario_liquido = cargo - descontos - total_beneficio;
 
       resultado.innerHTML =
-        "<p><b>Funcionário: </b> " +nome + "</p>" +
-        "<p><b>Salário Base: </b>R$ " +cargo.toFixed(2).replace(".", ",") + "</p>"+
-        "<p><b>Benefícios: </b>R$ " +total_beneficio.toFixed(2).replace(".", ",") + "</p>"+
-        "<p><b>Descontos (" +porcentagem_desconto +"%): </b> R$ " +descontos.toFixed(2).replace(".", ",") + "</p>"+
-        "<p><b>Salário Líquido: </b>R$ " +salario_liquido.toFixed(2).replace(".", ",") +"</p>";
+        "<p><b>Funcionário: </b> " +
+        nome +
+        "</p>" +
+        "<p><b>Salário Base: </b>R$ " +
+        cargo.toFixed(2).replace(".", ",") +
+        "</p>" +
+        "<p><b>Benefícios: </b>R$ " +
+        total_beneficio.toFixed(2).replace(".", ",") +
+        "</p>" +
+        "<p><b>Descontos (" +
+        porcentagem_desconto +
+        "%): </b> R$ " +
+        descontos.toFixed(2).replace(".", ",") +
+        "</p>" +
+        "<p><b>Salário Líquido: </b>R$ " +
+        salario_liquido.toFixed(2).replace(".", ",") +
+        "</p>";
     }
   }
 }
